b535f900c37364fc6b33c49a5850b638c20d45f8
Adarsh T <adarshadhu003@gmail.com>
2026-09-14 16:41:27 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Agricultural-Regionalization-Of-India/',
   plugins: [react()],
   server: {
     port: 3000,
